53ac1b249b0b28ea4aea3c91507ce141a57f3218
Programming Web <pwd@NVTHS1.PRVT>
2018-11-28 09:06:21 -0800

    next commit

---
+++ WarSpace.py
@@ -0,0 +1,60 @@
+import random
+
+#Import the Turtle module
+import turtle
+turtle.fd(0)
+turtle.speed(0)
+turtle.bgcolor("black")
+turtle.ht()
+turtle.setundobuffer(1)
+turtle.tracer(1)
+
+class Sprite(turtle.Turtle):
+    def __init__(self, spriteshape,color,startx,starty):
+        turtle.Turtle.__init__(self, shape = spriteshape)
+        self.speed(0)
+        self.penup()
+        self.color(color)
+        self.fd(0)
+        self.goto(startx, starty)
+        self.speed = 1
+
+    def move(self):
+        self.fd(self.speed)
+
+class Player(Sprite):
+    def __init__(self, spriteshape, color, startx, starty):
+        Sprite.__init__(self, spriteshape, color, startx, starty)
+        self.speed = 4
+        self.lives = 3
+
+    def turn_left(self):
+        self.lt(22.5)
+
+    def turn_right(self):
+        self.rt(22.5)
+
+    def accelerate(self):
+        self.speed += 1
+
+    def decelerate(self):
+        self.speed -= 1
+
+#Create my sprites
+player = Player("triangle", "white", 0, 0)
+
+#Keyboard bindings
+turtle.onkey(player.turn_left, "Left" )
+turtle.onkey(player.turn_right, "Right" )
+turtle.onkey(player.accelerate, "Up" )
+turtle.onkey(player.decelerate, "Down" )
+turtle.listen()
+
+#Main game loop
+while True:
+    player.move()
+
+
+
+
+delay = raw_input("Press enter to finish. > ")
